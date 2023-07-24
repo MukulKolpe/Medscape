2ac80f3647291e059fe3d3dc470beac01b18f62e
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-07-24 21:20:09 +0530

    Profile Page UI

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,8 +1,37 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import userabijson from "../../utils/usersideabi.json";
 import { ethers } from "ethers";
 import { useAuth } from "@polybase/react";
+import { useRouter } from "next/router";
 
+import {
+  Box,
+  VStack,
+  Button,
+  Flex,
+  Divider,
+  chakra,
+  Grid,
+  GridItem,
+  Container,
+} from "@chakra-ui/react";
+
+interface DataProps {
+  heading: string;
+  text: string;
+}
+const Feature = ({ heading, text }: DataProps) => {
+  return (
+    <GridItem>
+      <chakra.h3 fontSize="xl" fontWeight="600">
+        {heading}
+      </chakra.h3>
+      <chakra.p>{text}</chakra.p>
+    </GridItem>
+  );
+};
 const Profile = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -14,7 +43,7 @@ const Profile = () => {
   const [isDiabetic, setIsDiabetic] = useState(true);
   const [hasHighBP, setHasHighBP] = useState(true);
   const [hasDisability, setHasDisability] = useState(true);
-
+  const router = useRouter();
   const { auth, state } = useAuth();
 
   useEffect(() => {
@@ -55,10 +84,62 @@ const Profile = () => {
 
   return (
     <div>
-      <p>
-        {name}, {email}, {aadharNo}, {gender}, {age}, {bloodGrp}, {dob},
-        {String(isDiabetic)}, {String(hasHighBP)}, {String(hasDisability)}
-      </p>
+      <Box as={Container} maxW="7xl" mt={14} p={4}>
+        <Grid
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            sm: "repeat(2, 1fr)",
+            md: "repeat(2, 1fr)",
+          }}
+          gap={4}
+        >
+          <GridItem colSpan={3}>
+            <VStack alignItems="flex-start" spacing="20px">
+              <chakra.h2 fontSize="3xl" fontWeight="700">
+                Welcome, {name} to your personalised dashboard!
+              </chakra.h2>
+            </VStack>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <Button
+              colorScheme="green"
+              size="md"
+              onClick={() => router.push("/book")}
+            >
+              Book an appointment
+            </Button>
+          </GridItem>
+        </Grid>
+        <Divider mt={12} mb={12} />
+        <Grid
+          templateColumns={{
+            base: "repeat(1, 1fr)",
+            sm: "repeat(2, 1fr)",
+            md: "repeat(4, 1fr)",
+          }}
+          gap={{ base: "8", sm: "12", md: "16" }}
+        >
+          <Feature heading={"Name"} text={name} />
+          <Feature heading={"Email Address"} text={email} />
+          <Feature heading={"Gender"} text={gender} />
+          <Feature heading={"Age"} text={age.toString()} />
+          <Feature heading={"Date of Birth"} text={dob} />
+          <Feature heading={"Aadhar Number"} text={aadharNo} />
+          <Feature heading={"Blood Group"} text={bloodGrp} />
+          <Feature
+            heading={"Diabetic Tendencies"}
+            text={isDiabetic ? "Yes" : "No"}
+          />
+          <Feature
+            heading={"High Blood Pressure"}
+            text={hasHighBP ? "Yes" : "No"}
+          />
+          <Feature
+            heading={"Disabilities"}
+            text={hasDisability ? "Yes" : "No"}
+          />
+        </Grid>
+      </Box>
     </div>
   );
 };
