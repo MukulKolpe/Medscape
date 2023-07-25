00ca9a1bc07781b4babcacb114c3e68de187d055
Mukul Kolpe <mukulkolpe45@gmail.com>
2023-07-25 08:10:50 +0530

    Connected Doctor Registration form with the contract + Toast notifications for IPFS success and failures

--- src/components/DoctorRegistration/Form.tsx
+++ src/components/DoctorRegistration/Form.tsx
@@ -34,10 +34,29 @@ import { useToast } from "@chakra-ui/react";
 import { useAuth } from "@polybase/react";
 import { stringify } from "querystring";
 import { Clicker_Script } from "next/font/google";
+import { ethers } from "ethers";
+import createdoctorabi from "../../utils/createdoctorabi.json";
 
-const Form1 = () => {
+const Form1 = ({ getName, getEmail, getGender, getAge }) => {
   const [show, setShow] = React.useState(false);
   const handleClick = () => setShow(!show);
+
+  const handleName = (e) => {
+    getName(e);
+  };
+
+  const handleEmail = (e) => {
+    getEmail(e);
+  };
+
+  const handleGender = (e) => {
+    getGender(e);
+  };
+
+  const handleAge = (e) => {
+    getAge(e);
+  };
+
   return (
     <>
       <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
@@ -48,7 +67,12 @@ const Form1 = () => {
         <FormLabel htmlFor="name" fontWeight={"normal"}>
           Name
         </FormLabel>
-        <Input id="name" placeholder="Name" autoComplete="name" />
+        <Input
+          id="name"
+          placeholder="Name"
+          autoComplete="name"
+          onChange={(e) => handleName(e.target.value)}
+        />
       </FormControl>
 
       <FormControl mt="2%">
@@ -60,6 +84,7 @@ const Form1 = () => {
           type="email"
           placeholder="doctor@email.com"
           autoComplete="email"
+          onChange={(e) => handleEmail(e.target.value)}
         />
         <FormHelperText>We'll never share your email.</FormHelperText>
       </FormControl>
@@ -86,6 +111,7 @@ const Form1 = () => {
           size="sm"
           w="full"
           rounded="md"
+          onChange={(e) => handleGender(e.target.value)}
         >
           <option>Male</option>
           <option>Female</option>
@@ -97,7 +123,12 @@ const Form1 = () => {
         <FormLabel htmlFor="age" fontWeight={"normal"}>
           Age
         </FormLabel>
-        <NumberInput step={1} defaultValue={18} min={1}>
+        <NumberInput
+          step={1}
+          defaultValue={18}
+          min={1}
+          onChange={(value) => handleAge(value)}
+        >
           <NumberInputField />
           <NumberInputStepper>
             <NumberIncrementStepper />
@@ -109,10 +140,12 @@ const Form1 = () => {
   );
 };
 
-const Form2 = () => {
+const Form2 = ({ getDisplayImage, getDate, getYoe }) => {
+  const toast = useToast();
   const inputRef = useRef(null);
   const { state } = useAuth();
   const [displayImage, setDisplayImage] = useState();
+  const [ipfsUrl, setIpfsUrl] = useState("");
   const changeHandler = () => {
     setDisplayImage(inputRef.current?.files[0]);
   };
@@ -132,8 +165,39 @@ const Form2 = () => {
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
+        console.log(response.ipfs_url);
+        setIpfsUrl(response.ipfs_url);
+        if (displayImage) {
+          toast({
+            title: "Display Image Uploaded to the IPFS.",
+            description: "Congratulations 🎉 ",
+            status: "success",
+            duration: 1000,
+            isClosable: true,
+            position: "top-right",
+          });
+        } else {
+          toast({
+            title: "Display Image not Uploaded to the IPFS.",
+            description: "Please attach the degree certificate ",
+            status: "error",
+            duration: 1000,
+            isClosable: true,
+            position: "top-right",
+          });
+        }
       })
       .catch((err) => console.error(err));
+  };
+
+  getDisplayImage(ipfsUrl);
+
+  const handleDate = (e) => {
+    getDate(e);
+  };
+
+  const handleYoe = (e) => {
+    getYoe(e);
   };
 
   return (
@@ -141,135 +205,144 @@ const Form2 = () => {
       <Heading w="100%" textAlign={"center"} fontWeight="normal" mb="2%">
         Personal Details
       </Heading>
+      <SimpleGrid columns={1} spacing={6}>
+        <FormControl mt="2%">
+          <FormLabel
+            fontWeight={"normal"}
+            color="gray.700"
+            _dark={{
+              color: "gray.50",
+            }}
+          >
+            Upload Display Image
+          </FormLabel>
 
-      <FormControl mt="2%">
-        <FormLabel
-          fontWeight={"normal"}
-          color="gray.700"
-          _dark={{
-            color: "gray.50",
-          }}
-        >
-          Upload Display Image
-        </FormLabel>
-
-        <Flex
-          mt={1}
-          justify="center"
-          px={6}
-          pt={5}
-          pb={6}
-          borderWidth={2}
-          _dark={{
-            color: "gray.500",
-          }}
-          borderStyle="dashed"
-          rounded="md"
-        >
-          <Stack spacing={1} textAlign="center">
-            <Icon
-              mx="auto"
-              boxSize={12}
-              color="gray.400"
-              _dark={{
-                color: "gray.500",
-              }}
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </Icon>
-            <Text>{displayImage?.name}</Text>
-            <Flex
-              fontSize="sm"
-              color="gray.600"
-              _dark={{
-                color: "gray.400",
-              }}
-              alignItems="baseline"
-            >
-              <chakra.label
-                htmlFor="file-upload"
-                cursor="pointer"
-                rounded="md"
-                fontSize="md"
-                color="brand.600"
+          <Flex
+            mt={1}
+            justify="center"
+            px={6}
+            pt={5}
+            pb={6}
+            borderWidth={2}
+            _dark={{
+              color: "gray.500",
+            }}
+            borderStyle="dashed"
+            rounded="md"
+          >
+            <Stack spacing={1} textAlign="center">
+              <Icon
+                mx="auto"
+                boxSize={12}
+                color="gray.400"
                 _dark={{
-                  color: "brand.200",
+                  color: "gray.500",
                 }}
-                pos="relative"
-                _hover={{
-                  color: "brand.400",
-                  _dark: {
-                    color: "brand.300",
-                  },
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 48 48"
+                aria-hidden="true"
+              >
+                <path
+                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </Icon>
+              <Text>{displayImage?.name}</Text>
+              <Flex
+                fontSize="sm"
+                color="gray.600"
+                _dark={{
+                  color: "gray.400",
+                }}
+                alignItems="baseline"
+              >
+                <chakra.label
+                  htmlFor="file-upload"
+                  cursor="pointer"
+                  rounded="md"
+                  fontSize="md"
+                  color="brand.600"
+                  _dark={{
+                    color: "brand.200",
+                  }}
+                  pos="relative"
+                  _hover={{
+                    color: "brand.400",
+                    _dark: {
+                      color: "brand.300",
+                    },
+                  }}
+                >
+                  <span>{"Upload an Image"}</span>
+                  <VisuallyHidden>
+                    <input
+                      id="file-upload"
+                      name="file-upload"
+                      type="file"
+                      ref={inputRef}
+                      onChange={changeHandler}
+                    />
+                  </VisuallyHidden>
+                </chakra.label>
+                <Text pl={1}>or drag and drop</Text>
+              </Flex>
+              <Text
+                fontSize="xs"
+                color="gray.500"
+                _dark={{
+                  color: "gray.50",
                 }}
               >
-                <span>{"Upload an Image"}</span>
-                <VisuallyHidden>
-                  <input
-                    id="file-upload"
-                    name="file-upload"
-                    type="file"
-                    ref={inputRef}
-                    onChange={changeHandler}
-                  />
-                </VisuallyHidden>
-              </chakra.label>
-              <Text pl={1}>or drag and drop</Text>
-            </Flex>
-            <Text
-              fontSize="xs"
-              color="gray.500"
-              _dark={{
-                color: "gray.50",
-              }}
-            >
-              PNG, JPG, GIF up to 10MB
-            </Text>
-          </Stack>
-        </Flex>
-      </FormControl>
+                PNG, JPG, GIF up to 10MB
+              </Text>
+            </Stack>
+          </Flex>
+        </FormControl>
 
-      <Button onClick={uploadIPFS}>Upload to IPFS</Button>
+        <Button onClick={uploadIPFS}>Upload to IPFS</Button>
 
-      <FormControl mr="5%" mt="2%">
-        <FormLabel htmlFor="datetime-local" fontWeight={"normal"}>
-          Date of Birth
-        </FormLabel>
-        <Input
-          placeholder="Select Date and Time"
-          size="md"
-          type="date"
-          id="datetime-local"
-        />
-      </FormControl>
-      <FormControl mr="5%" mt="2%">
-        <FormLabel htmlFor="yoe" fontWeight={"normal"}>
-          Total years of experience
-        </FormLabel>
-        <NumberInput step={1} defaultValue={0} min={1}>
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-      </FormControl>
+        <FormControl mr="5%" mt="2%">
+          <FormLabel htmlFor="datetime-local" fontWeight={"normal"}>
+            Date of Birth
+          </FormLabel>
+          <Input
+            placeholder="Select Date and Time"
+            size="md"
+            type="date"
+            id="datetime-local"
+            onChange={(e) => handleDate(e.target.value)}
+          />
+        </FormControl>
+        <FormControl mr="5%" mt="2%">
+          <FormLabel htmlFor="yoe" fontWeight={"normal"}>
+            Total years of experience
+          </FormLabel>
+          <NumberInput
+            step={1}
+            defaultValue={0}
+            min={1}
+            onChange={(value) => handleYoe(value)}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </FormControl>
+      </SimpleGrid>
     </>
   );
 };
 
-const Form3 = () => {
+const Form3 = ({ getDegree, getLicenseNo, getSpec }) => {
+  const toast = useToast();
   const inputRef = useRef(null);
   const [displayImage, setDisplayImage] = useState();
+  const [ipfsUrl, setIpfsUrl] = useState("");
   const changeHandler = () => {
     setDisplayImage(inputRef.current?.files[0]);
   };
@@ -289,8 +362,38 @@ const Form3 = () => {
       .then((response) => response.json())
       .then((response) => {
         console.log(response);
+        setIpfsUrl(response.ipfs_url);
+        if (displayImage) {
+          toast({
+            title: "Degree Certificate Uploaded to the IPFS.",
+            description: "Congratulations 🎉 ",
+            status: "success",
+            duration: 1000,
+            isClosable: true,
+            position: "top-right",
+          });
+        } else {
+          toast({
+            title: "Degree Certificate not Uploaded to the IPFS.",
+            description: "Please attach the degree certificate ",
+            status: "error",
+            duration: 1000,
+            isClosable: true,
+            position: "top-right",
+          });
+        }
       })
       .catch((err) => console.error(err));
+  };
+
+  getDegree(ipfsUrl);
+
+  const handleLicenseNo = (e) => {
+    getLicenseNo(e);
+  };
+
+  const handleSpec = (e) => {
+    getSpec(e);
   };
   return (
     <>
@@ -393,12 +496,18 @@ const Form3 = () => {
             </Stack>
           </Flex>
         </FormControl>
+
         <Button onClick={uploadIPFS}>Upload to IPFS</Button>
+
         <FormControl mr="2%">
           <FormLabel htmlFor="license_number" fontWeight={"normal"}>
             License Number
           </FormLabel>
-          <Input id="license_number" placeholder="000000" />
+          <Input
+            id="license_number"
+            placeholder="000000"
+            onChange={(e) => handleLicenseNo(e.target.value)}
+          />
         </FormControl>
         <FormControl>
           <FormLabel
@@ -422,6 +531,7 @@ const Form3 = () => {
             size="sm"
             w="full"
             rounded="md"
+            onChange={(e) => handleSpec(e.target.value)}
           >
             <option>Anesthesiology</option>
             <option>Cardiology</option>
@@ -459,6 +569,58 @@ export default function Multistep() {
   const toast = useToast();
   const [step, setStep] = useState(1);
   const [progress, setProgress] = useState(33.33);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [gender, setGender] = useState("");
+  const [age, setAge] = useState(18);
+  const [displayImage, setDisplayImage] = useState();
+  const [date, setDate] = useState("");
+  const [yoe, setYoe] = useState(0);
+  const [degree, setDegree] = useState();
+  const [licenseNo, setLicenseNo] = useState("");
+  const [spec, setSpec] = useState("");
+
+  const { state } = useAuth();
+  const handleSubmit = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(
+      "0xfFc691e5B23633A683E82fad9dAd858bD1B9875D",
+      createdoctorabi,
+      signer
+    );
+    console.log(state?.userId);
+    console.log(degree);
+    console.log(licenseNo);
+    const tx = await contract.createDoctor(
+      name,
+      age,
+      displayImage,
+      yoe,
+      date,
+      spec,
+      degree,
+      licenseNo,
+      state?.userId,
+      email,
+      gender
+    );
+
+    console.log(degree);
+    console.log(licenseNo);
+    await tx.wait();
+
+    toast({
+      title: "Account created.",
+      description:
+        "Congratulations 🎉 your details are submitted, we'll get back to you soon!",
+      status: "success",
+      duration: 3000,
+      isClosable: true,
+      position: "top-right",
+    });
+  };
+
   return (
     <>
       <Box
@@ -477,7 +639,26 @@ export default function Multistep() {
           mx="5%"
           isAnimated
         ></Progress>
-        {step === 1 ? <Form1 /> : step === 2 ? <Form2 /> : <Form3 />}
+        {step === 1 ? (
+          <Form1
+            getName={(q) => setName(q)}
+            getEmail={(q) => setEmail(q)}
+            getGender={(q) => setGender(q)}
+            getAge={(q) => setAge(q)}
+          />
+        ) : step === 2 ? (
+          <Form2
+            getDisplayImage={(q) => setDisplayImage(q)}
+            getDate={(q) => setDate(q)}
+            getYoe={(q) => setYoe(q)}
+          />
+        ) : (
+          <Form3
+            getDegree={(q) => setDegree(q)}
+            getLicenseNo={(q) => setLicenseNo(q)}
+            getSpec={(q) => setSpec(q)}
+          />
+        )}
         <ButtonGroup mt="5%" w="100%">
           <Flex w="100%" justifyContent="space-between">
             <Flex>
@@ -517,14 +698,7 @@ export default function Multistep() {
                 colorScheme="red"
                 variant="solid"
                 onClick={() => {
-                  toast({
-                    title: "Account created.",
-                    description:
-                      "Congratulations 🎉 your details are submitted, we'll get back to you soon!",
-                    status: "success",
-                    duration: 3000,
-                    isClosable: true,
-                  });
+                  handleSubmit();
                 }}
               >
                 Submit
